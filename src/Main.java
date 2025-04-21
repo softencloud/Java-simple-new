6cc994aacb985a568876dc08a156250a8912ecf0
anujohnk <anuanujohnk@gmail.com>
2025-04-21 10:22:06 +0530

    second commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Date currentDate = new Date();
-        System.out.println("Hello, soften!, Let's learn DevOps!!! Current date: " + currentDate);
+        System.out.println("Hello, soften!, Let's learn DevOps new!!! Current date: " + currentDate);
     }
 }
